a8a05d928867e763fc98f5caa0b84749fe1ff7dd
Johan Wassberg <jocar@sunet.se>
2024-06-20 08:00:20 +0200

    Guess they changed cachedir

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ if [ ! -f /var/www/var/letswifi-dev.sqlite ]; then
     sqlite3 /var/www/var/letswifi-dev.sqlite < /var/www/sql/letswifi.sqlite.sql
 fi
 
-mkdir -p /var/simplesamlphp/mdq-cache
+mkdir -p /var/cache/simplesamlphp
 
-chown www-data:www-data /var/simplesamlphp/mdq-cache
+chown www-data:www-data /var/cache/simplesamlphp
 
 env APACHE_LOCK_DIR=/var/lock/apache2 APACHE_RUN_DIR=/var/run/apache2 APACHE_PID_FILE=/var/run/apache2/apache2.pid APACHE_RUN_USER=www-data APACHE_RUN_GROUP=www-data APACHE_LOG_DIR=/var/log/apache2 apache2 -DFOREGROUND
